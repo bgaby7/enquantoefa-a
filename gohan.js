f69aab9c2d88ff9b91da62c11d113e07ea9c0115
gabriela <125597208+bgaby7@users.noreply.github.com>
2023-04-13 15:56:26 -0300

    Add files via upload

---
+++ gohan.js
@@ -0,0 +1,46 @@
+function Enquanto(){
+    var user;
+    var password;
+    var recorrente;
+    recorrente = true;
+    do {
+        window.alert("Digite o seu nome de usuário!");
+        user = window.prompt('Enter a value for user');
+        window.alert("Digite sua senha!");
+        password = window.prompt('Enter a value for password');
+        if (user == "gaby") {
+            if (password == "1234") {
+                window.alert("você será redirecionado ao seu perfil.");
+                recorrente = false;
+            } else {
+                window.alert("Usuário ou senha inválido.");
+            }
+        } else {
+            window.alert("usuário ou senha inválido.");
+        }
+    } while (recorrente == true);
+}
+
+function Faça() {
+    var user;
+    var password;
+    var recorrente;
+    
+    recorrente = true;
+    do {
+        window.alert("Digite o seu nome de usuário!");
+        user = window.prompt('Enter a value for user');
+        window.alert("Digite sua senha!");
+        password = window.prompt('Enter a value for password');
+        if (user == "gaby") {
+            if (password == "1234") {
+                window.alert("Você será redirecionado ao seu perfil.");
+                recorrente = false;
+            } else {
+                window.alert("Usuário ou senha inválido.");
+            }
+        } else {
+            window.alert("Usuário ou senha inválido.");
+        }
+    } while (recorrente == true);
+}
